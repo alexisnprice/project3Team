174ba924ca6c9a67851d047c14f5f9abff057f4e
alexisnprice <pricenalexis@gmail.com>
2019-12-10 00:41:09 -0500

    correct login setup

--- project3Local/reviewDBase.php
+++ project3Local/reviewDBase.php
@@ -1,8 +1,11 @@
-<?php
+<?php include 'header.php';
+//equiv to processing page
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["restaurant_name"];
     $type = $_POST["restaurant_type"];
-    $target_dir = "images/";
+    $rate = $_POST["restaurant_rating"];
+    $comment = $_POST["restaurant_comment"];
+    $target_dir = "imgs/";
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
     move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],$target_file);
 
@@ -12,24 +15,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     $current = file_get_contents($file);
-    $current .= "$name, $type,$target_file\n";
+    $current .= "$name, $type,$rate,$comment,$target_file\n";
     file_put_contents($file,$current);
 ?>
     <main id="main">
-    <table>
-        <tr>
-            <td>Restaurant Name</td>
-            <td><?php echo $name; ?></td>
-        </tr>
-        <tr>
-            <td>Type</td>
-            <td><?php echo $type; ?></td>
-        </tr>
-        <tr>
-            <td>Image</td>
-            <td style="height:200px;width:100px;"><?php echo "<img style='height: 100%; width:100%;"?> </td>
-        </tr>
-    </table>
+        <p>Review submitted successfully!</p>
+</main>
 
 
 <?php
